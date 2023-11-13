0ef97bee627265555e16b871d32f6958db214384
Danciu Alex <dersummoner@gmail.com>
2023-11-13 15:02:27 +0200

    Added escaping to printf and sprintf

--- strconst.asm
+++ strconst.asm
@@ -15,26 +15,26 @@ section .text
 _start:
     GetStdHandle(STD_OUTPUT_HANDLE, [hStdOut])
 
-printf([hStdOut], roStr_0)
-printf([hStdOut], roStr_1)
-printf([hStdOut], roStr_2)
-printf([hStdOut], roStr_3)
-printf([hStdOut], roStr_4)
-printf([hStdOut], roStr_5)
-printf([hStdOut], roStr_6)
-printf([hStdOut], roStr_7)
-printf([hStdOut], roStr_8)
-printf([hStdOut], roStr_9)
+    printf([hStdOut], roStr_0)
+    printf([hStdOut], roStr_1)
+    printf([hStdOut], roStr_2)
+    printf([hStdOut], roStr_3)
+    printf([hStdOut], roStr_4)
+    printf([hStdOut], roStr_5)
+    printf([hStdOut], roStr_6)
+    printf([hStdOut], roStr_7)
+    printf([hStdOut], roStr_8)
+    printf([hStdOut], roStr_9)
 
     ExitProcess(0)
 section .rdata
-    roStr_9 db "    ExitProcess(0)", 0
-    roStr_8 db "", 0
-    roStr_7 db "    printf([hStdOut], roStr_1)", 0
-    roStr_6 db "    printf([hStdOut], roStr_0)", 0
-    roStr_5 db "", 0
-    roStr_4 db "    GetStdHandle(STD_OUTPUT_HANDLE, [hStdOut])", 0
-    roStr_3 db "_start:", 0
-    roStr_2 db "", 0
-    roStr_1 db "    global _start", 0
-    roStr_0 db "section .text", 0
+    roStr_9 db "    ExitProcess(0)\r\n", 0
+    roStr_8 db "\r\n", 0
+    roStr_7 db "    printf([hStdOut], roStr_1)\r\n", 0
+    roStr_6 db "    printf([hStdOut], roStr_0)\r\n", 0
+    roStr_5 db "\r\n", 0
+    roStr_4 db "    GetStdHandle(STD_OUTPUT_HANDLE, [hStdOut])\r\n", 0
+    roStr_3 db "_start:\r\n", 0
+    roStr_2 db "\r\n", 0
+    roStr_1 db "    global _start\r\n", 0
+    roStr_0 db "section .text\r\n", 0
